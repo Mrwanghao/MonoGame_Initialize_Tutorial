05159b20ee6974594c1d1d6ce085617b4f5cc39a
Mrwanghao <845811521@qq.com>
2019-03-17 11:48:55 +0800

    Initialize

---
+++ MonoGame_Initialize_Tutorial/MainGame.cs
@@ -0,0 +1,12 @@
+﻿using Nez;
+
+namespace MonoGame_Initialize_Tutorial.MacOS
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MainGame : Core
+    {
+
+    }
+}
---
+++ MonoGame_Initialize_Tutorial/MainScene.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MonoGame_Initialize_Tutorial
+{
+    public class MainScene
+    {
+        public MainScene()
+        {
+        }
+    }
+}
